6ec3db59949d37743e878669a86dc249a8fe9605
James <contato@jameskjr.com>
2024-10-06 18:18:07 -0300

    first

---
+++ index_redirecionar.php
@@ -0,0 +1,10 @@
+<?php
+// Define a URL de destino
+$url_destino = 'https://bevart.tech/app/src/login/views/login';
+
+// Redireciona para a URL de destino
+header('Location: ' . $url_destino);
+
+// Encerra o script para garantir que nenhum código adicional seja executado
+exit();
+?>
